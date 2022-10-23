6ba7e4f1a394375084ce629742572dd3930d9183
asraf-code <64000853+asraf-code@users.noreply.github.com>
2022-10-24 02:19:04 +0530

    completed translation app

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output-box");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "	https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 }
 
 function errorHandler(error) {
@@ -15,16 +15,17 @@ function errorHandler(error) {
 
 function clickHandler() {
     // outputDiv.innerText = "lhaldhla" + txtInput.value;
+
     var inputText = txtInput.value; // taking input
 
     // calling server for processing
     fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
-    .catch(errorHandler)
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-
-
-
