7052f4826b518b061ec580201f9f740ae73ce8c5
asraf-code <64000853+asraf-code@users.noreply.github.com>
2022-10-24 00:51:28 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output-box");
 
 
 function clickHandler() {
-    
-    console.log("clicked!");
-    console.log("input", txtInput.value);
-}
+    outputDiv.innerText = "ajlajnfolj " + txtInput.value;
+};
 
 btnTranslate.addEventListener("click", clickHandler)
 
