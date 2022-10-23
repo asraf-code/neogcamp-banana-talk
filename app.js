44e2b1d588dac78522a0df19bf01866053b33e66
asraf-code <64000853+asraf-code@users.noreply.github.com>
2022-10-23 15:16:03 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
 
 function clickHandler() {
     
     console.log("clicked!");
-
+    console.log("input", txtInput.value);
 }
 
 btnTranslate.addEventListener("click", clickHandler)
